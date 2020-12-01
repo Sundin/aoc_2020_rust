9e54c72c32719033fa821861abc82c3277db6ea8
Gustav Sundin <sundin666@gmail.com>
2020-12-01 08:28:45 +0100

    Implementation and unit tests

--- src/expense.rs
+++ src/expense.rs
@@ -1,5 +1,19 @@
-pub fn find_entries_with_sum(contents: &Vec<i32>, sum: i32) -> i32 {
-    0
+pub fn find_entries_with_sum(contents: &Vec<i32>, sum: i32) -> (i32, i32) {
+    let length = contents.len();
+    for x in 0..length {
+        for y in 0..length {
+            if contents[x] + contents[y] == 2020 {
+                println!("{} + {} = 2020", contents[x], contents[y]);
+                return (contents[x], contents[y]);
+            }
+        }
+    }
+    (0,0)
+}
+
+pub fn multiply_entries_with_sum(contents: &Vec<i32>, sum: i32) -> i32 {
+    let (x, y) = find_entries_with_sum(&contents, sum);
+    x * y
 }
 
 #[cfg(test)]
@@ -7,8 +21,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_multiple_rounds() {
-        let v: Vec<i32> = vec![1,1,1,4,99,5,6,0,99];
-        assert_eq!(0, find_entries_with_sum(&v, 0));
+    fn test_find_entries() {
+        let v: Vec<i32> = vec![1721,979,366,299,675,1456];
+        assert_eq!((1721, 299), find_entries_with_sum(&v, 0));
+    }
+
+    #[test]
+    fn test_multiply_entries() {
+        let v: Vec<i32> = vec![1721,979,366,299,675,1456];
+        assert_eq!(514579, multiply_entries_with_sum(&v, 0));
     }
 }
