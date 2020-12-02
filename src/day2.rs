f215dae275fb8a79fd0687c5174a15a51b2a9e92
Gustav Sundin <sundin666@gmail.com>
2020-12-02 07:47:44 +0100

    Check password

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,17 @@
 
 fn check_password(password: &str, min: i32, max: i32, letter: char) -> bool {
-    true
+    let occurences = password.matches(letter).count() as i32;
+    occurences >= min && occurences <= max
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_password() {
+        assert_eq!(true, check_password("abcde", 1, 3, 'a'));
+        assert_eq!(true, check_password("ccccccccc", 2, 9, 'c'));
+        assert_eq!(false, check_password("cdefg", 1, 3, 'b'));
+    }
 }
