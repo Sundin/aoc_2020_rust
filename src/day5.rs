2d4546864b39598c5db754de08bd454632817da3
Gustav Sundin <sundin666@gmail.com>
2020-12-05 10:28:00 +0100

    Boarding pass struct

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,30 @@
+use std::str::FromStr;
+use std::num::ParseIntError;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct BoardingPass {
+    pub binary: String,
+    pub seat_id: i32,
+}
+
+impl FromStr for BoardingPass {
+    type Err = ParseIntError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut seat_id = 0;
+        for c in input.chars().rev() {
+            println!("char: {}", c);
+        }
+
+        Ok(BoardingPass { binary: input.to_string(), seat_id: seat_id })
+    }
+}
+
 pub fn part_1(input: &str) -> i64 {
+    for line in input.lines() {
+        let boarding_pass: BoardingPass = line.parse().unwrap();
+        println!("bp: {}", boarding_pass.seat_id);
+    }
     0
 }
 
@@ -6,7 +32,7 @@ pub fn part_2(input: &str) -> i64 {
     0
 }
 
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
     use super::*;
     use super::super::files;
@@ -14,12 +40,12 @@ mod tests {
     #[test]
     fn test_part_1() {
         let contents = files::get_file_contents("test_input/day5.txt".to_owned()).unwrap();
-        assert_eq!(0, part_1(input: &str))
+        assert_eq!(0, part_1(&contents))
     }
 
     #[test]
     fn test_part_2() {
         let contents = files::get_file_contents("test_input/day5.txt".to_owned()).unwrap();
-        assert_eq!(0, part_2(input: &str))
+        assert_eq!(0, part_2(&contents))
     }
 }
